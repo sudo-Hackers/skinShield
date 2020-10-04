f9f6b8ad110af94f3914b5fff609957fc32973e1
saiprava <rsaiprava99@gmail.com>
2020-10-04 10:39:25 +0530

    Doctor side reports

---
+++ client/src/components/doctorComp/Photo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Photo = ({ id, report, date }) => {
+  const url = `${process.env.REACT_APP_LINK}/${id}`;
+  console.log(url);
+  return (
+    <Card className="photo" >
+      <Card.Img
+        variant="top"
+        src={url}
+        alt="Photo"
+      />
+      <Card.Text>Cancer Type: {report}</Card.Text>
+      <Card.Text>Date: {date}</Card.Text>
+    </Card>
+  );
+};
+
+export default Photo;
